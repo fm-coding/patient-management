0bf7b21d11f84e69a76e84fbc0a6127214266463
fm-coding <133166438+fm-coding@users.noreply.github.com>
2025-04-08 12:13:43 +0300

    updated patient-service

--- patient-service/src/main/java/com/pm/patientservice/exception/GlobalExceptionHandler.java
+++ patient-service/src/main/java/com/pm/patientservice/exception/GlobalExceptionHandler.java
@@ -26,6 +26,14 @@ public class GlobalExceptionHandler {
         Map<String, String> errors = new HashMap<>();
         errors.put("message","Email already exists");
         return ResponseEntity.badRequest().body(errors);
+    }
 
+    @ExceptionHandler(PatientNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handlePatientNotFoundException(PatientNotFoundException ex) {
+        log.warn("Patient not found: {}", ex.getMessage());
+
+        Map<String, String> errors = new HashMap<>();
+        errors.put("message","Patient not found");
+        return ResponseEntity.badRequest().body(errors);
     }
 }
